bcfdc52288646bc605ffc8057d0a1082950d4a04
ERIK_WITTEMEIER <erik.wittemeier@gmail.com>
2026-06-14 18:13:12 +0200

    feat: added a Dockerfile for event-service

--- event-service/src/main.rs
+++ event-service/src/main.rs
@@ -14,7 +14,7 @@ async fn main() {
         .route("/events", post(create_event))
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
