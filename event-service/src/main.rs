2da2170f1256d950346911df960c8f15dcef0039
ERIK_WITTEMEIER <erik.wittemeier@gmail.com>
2026-06-24 21:03:37 +0200

    feat: add update/delete/get endpoints for events

--- event-service/src/main.rs
+++ event-service/src/main.rs
@@ -1,37 +1,60 @@
-use axum::{Json, Router};
-use axum::routing::post;
-use std::sync::{Arc, Mutex};
 use axum::extract::State;
+use axum::routing::{delete, get, post};
+use axum::{Json, Router};
 use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
     let repo = EventRepositoryInMemory { events: vec![] };
     let event_service = CreateEventService { repo };
 
-    let state = Arc::new(Mutex::new(ApplicationState {event_service}));
+    let state = Arc::new(Mutex::new(ApplicationState { event_service }));
     let app = Router::new()
         .route("/events", post(create_event))
+        .route("/events", get(get_events))
+        .route("/events", delete(delete_event))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn create_event(
-    State(state): State<Arc<Mutex<ApplicationState>>>,
-    Json(input): Json<CreateEventInput>,
-) {
-    let event = Event { id: input.id, name: input.name };
-    let _ = &mut state.lock().unwrap().event_service.create(event.id, event.name);
-}
-
 struct ApplicationState {
     event_service: CreateEventService<EventRepositoryInMemory>,
 }
 
+async fn create_event(
+    State(state): State<Arc<Mutex<ApplicationState>>>,
+    Json(input): Json<CreateEventInput>,
+) {
+    let event = Event {
+        id: input.id,
+        name: input.name,
+    };
+    let _ = &mut state
+        .lock()
+        .unwrap()
+        .event_service
+        .create(event.id, event.name);
+}
+
+async fn get_events(State(state): State<Arc<Mutex<ApplicationState>>>) -> Json<Vec<Event>> {
+    let events = &state.lock().unwrap().event_service.get_all();
+    Json(events.clone())
+}
+
+async fn delete_event(
+    State(state): State<Arc<Mutex<ApplicationState>>>,
+    Json(input): Json<DeleteEventInput>,
+) {
+    let _ = &mut state.lock().unwrap().event_service.delete(input.id);
+}
+
 trait CreateEventUseCase: Send + Sync {
     fn create(&mut self, id: i128, name: String) -> Result<Event, String>;
+    fn delete(&mut self, id: i128) -> Result<(), String>;
+    fn get_all(&self) -> Vec<Event>;
 }
 
 struct CreateEventService<R: EventRepository> {
@@ -46,10 +69,20 @@ impl<R: EventRepository> CreateEventUseCase for CreateEventService<R> {
             Err(e) => Err(e.clone()),
         }
     }
+
+    fn delete(&mut self, id: i128) -> Result<(), String> {
+        self.repo.delete(id)
+    }
+
+    fn get_all(&self) -> Vec<Event> {
+        self.repo.query_all()
+    }
 }
 
 trait EventRepository: Send + Sync {
     fn save(&mut self, event: &Event) -> Result<(), String>;
+    fn delete(&mut self, id: i128) -> Result<(), String>;
+    fn query_all(&self) -> Vec<Event>;
 }
 
 struct EventRepositoryInMemory {
@@ -61,6 +94,15 @@ impl EventRepository for EventRepositoryInMemory {
         self.events.push(event.clone());
         Ok(())
     }
+
+    fn delete(&mut self, id: i128) -> Result<(), String> {
+        self.events.retain(|event| event.id != id);
+        Ok(())
+    }
+
+    fn query_all(&self) -> Vec<Event> {
+        self.events.clone()
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -69,7 +111,12 @@ struct CreateEventInput {
     pub name: String,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct DeleteEventInput {
+    pub id: i128,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct Event {
     pub id: i128,
     pub name: String,
