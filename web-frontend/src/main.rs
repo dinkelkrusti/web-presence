a7acf460283ecc2e38e05b4d437d8191839fac91
ERIK_WITTEMEIER <erik.wittemeier@gmail.com>
2026-07-15 21:53:30 +0200

    feat(fe): add placeholders for frontend

--- web-frontend/src/main.rs
+++ web-frontend/src/main.rs
@@ -6,5 +6,28 @@ fn main() {
 
 #[component]
 fn app() -> Element {
-    rsx! {"Event Planning"}
+    rsx! {
+        HeaderComponent {}
+        EventComponent { name: "Recital", date: "19.06.2026"}
+        FooterComponent {}
+    }
+}
+
+#[component]
+fn HeaderComponent() -> Element {
+    rsx!(
+        h2 { "Event Planning"}
+    )
+}
+
+#[component]
+fn EventComponent(name: String, date: String) -> Element {
+    rsx! { "{date}: {name}"}
+}
+
+#[component]
+fn FooterComponent() -> Element {
+    rsx!(
+        div { "about" }
+    )
 }
